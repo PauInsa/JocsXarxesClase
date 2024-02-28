4d20c3fe1710992aab48a5c2203619b48f4117e3
Pau Insa <pau.insa@enti.cat>
2024-02-28 10:53:49 +0100

    Windows i L'Abel

--- XarxesClase/XarxesClase.cpp
+++ XarxesClase/XarxesClase.cpp
@@ -1,11 +1,14 @@
 #include <conio.h>
 #include <iostream>
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
 #include "ConsoleControl.h"
 #include "Chat.h"
+#include "ConnectionAbstraction/SocketsManager.h"
 
 void RunClient();
 void RunServer();
+void RunWindows();
 
 unsigned short port = 3000;
 
@@ -14,6 +17,8 @@ int main()
     std::cout << "Select" << std::endl << "Client -> C" << std::endl << "Server -> S" << std::endl;
 
     char mode = ' ';
+
+    RunWindows();
 
     do
     {
@@ -35,6 +40,9 @@ int main()
     default:
         break;
     }
+
+    
+
     while (true)
     {
 
@@ -56,4 +64,36 @@ void RunServer() {
     std::cout << "Server";
 
     Chat* chat = Chat::Server(port);
+}
+
+void RunWindows() {
+
+    sf::RenderWindow windows;
+
+    windows.create(sf::VideoMode(800, 600), "Chat");
+    windows.setFramerateLimit(60);
+
+    sf::Text lAbel;
+
+    sf::Font font;
+    font.loadFromFile("Minecraft.ttf");
+
+    lAbel.setFont(font);
+    lAbel.setCharacterSize(100);
+    lAbel.setFillColor(sf::Color::White);
+    lAbel.setString("L'Abel es label");
+    sf::Vector2u windowsSize = windows.getSize();
+    //lAbel.setPosition(windowsSize.x * 0.5, windowsSize.y * 0.5);
+    lAbel.setPosition(0.5, windowsSize.y * 0.5);
+
+    while (windows.isOpen())
+    {
+        windows.clear(sf::Color(rand() % 200, rand() % 200, rand() % 200));
+        
+        lAbel.setFillColor(sf::Color(rand()%200, rand() % 200, rand() % 200));
+
+
+        windows.draw(lAbel);
+        windows.display();
+    }
 }
